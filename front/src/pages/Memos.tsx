dc0042a045e4efaec6e8be298192d69bf0fc0aa4
西川健太 <kentawest83@gmail.com>
2021-02-23 09:08:25 +0900

    Removed import.

--- front/src/pages/Memos.tsx
+++ front/src/pages/Memos.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Header } from '../components/Header';
-import { Footer } from '../components/Footer';
 import { Memo } from '../components/Memo';
 import { AddButton } from '../components/AddButton';
 
