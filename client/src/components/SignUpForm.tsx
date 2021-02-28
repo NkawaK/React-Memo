51366339e6f86a8a59f6a2763c50cf59f62cb240
西川健太 <kentawest83@gmail.com>
2021-03-01 05:24:57 +0900

    Add signup form.

--- client/src/components/SignUpForm.tsx
+++ client/src/components/SignUpForm.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const SignUpForm: React.FC = () => {
   return (
-    <div>SignUpForm</div>
+    <div className='container'>
+      <h2 className='icon'>
+        <i className="fas fa-book"></i> メモ帳
+      </h2>
+      <div className='signup-form'>
+        <div className='username'>
+          <label>ユーザー名</label>
+          <input type='text'/>
+        </div>
+        <div className='email'>
+          <label>Email</label>
+          <input type='text'/>
+        </div>
+        <div className='password'>
+          <label>Password</label>
+          <input type='password'/>
+        </div>
+        <div className='submit-button'>
+          <Link to='#' className="btn btn-–large btn--navy">
+            アカウント作成
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 };
